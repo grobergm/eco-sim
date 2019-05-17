e985038e2c2cb854e75e3b34c0f777a9773987dc
Matt Groberg <grobergmg@gmail.com>
2019-05-17 13:42:31 -0700

    add content and basic layout

--- src/components/AddPlayers.js
+++ src/components/AddPlayers.js
@@ -1,8 +1,55 @@
 import React from 'react';
+import collonization from '../img/backgrounds/collonization-600.jpg';
+import competition from '../img/backgrounds/competition-600.jpg';
+
 
 function AddPlayers(props){
+	const choiceGrid={
+		display:'grid',
+		gridTemplateColumns:'50% 50%',
+		textAlign:'center'
+	}
+	const fullRow={
+		gridColumn:'span 2',
+	}
+	const flexCenter={
+		display:'flex',
+		justifyContent:'center'
+	}
 	return (
 		<div>
+			<div>
+				<div style={flexCenter}>
+					<div style={choiceGrid}>
+						<h1 style={fullRow}>Collonization Strategy</h1>
+						<div>
+							<p>Lorem Ipsum</p>
+							<p>Dolar Sit amet</p>
+							<p>Something something</p>
+						</div>
+						<img style={{width:'100%'}} src={collonization} />
+						<button style={fullRow}>Select</button>
+					</div>
+					<div style={choiceGrid}>
+						<h1 style={fullRow}>Competition Strategy</h1>
+						<div>
+							<p>Lorem Ipsum</p>
+							<p>Dolar Sit amet</p>
+							<p>Something something</p>
+						</div>
+						<img style={{width:'100%'}} src={competition} />
+						<button style={fullRow}>Select</button>
+					</div>
+				</div>
+			</div>
+			<div style={flexCenter}>
+				<input
+				type="text"
+				id="playerName"
+				placeholder="Name"
+				/>
+				<button>Add Player</button>
+			</div>
 		</div>
 	)
 }
