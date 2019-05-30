4d79d98355f759b73bd4020afb79d6ffa0b5e956
Matt Groberg <grobergmg@gmail.com>
2019-05-30 15:48:23 -0700

    add player detail

--- src/GameControl/components/Players/PlayerDetail.js
+++ src/GameControl/components/Players/PlayerDetail.js
@@ -10,7 +10,7 @@ function PlayerDetail({game,dispatch}){
 
 	const turnChanger=()=>{
 		dispatch(selectOrganism(null))
-		if (game.day===game.gameLength){
+		if (game.day===game.length){
 			dispatch(changeTurn('lastTurn'))
 		} else if(game.turn===game.players.length-1){
 			dispatch(changeTurn('lastPlayer'));
@@ -20,12 +20,21 @@ function PlayerDetail({game,dispatch}){
 	}
 
 	const grid={
+		position:'fixed',
+		backgroundColor:'white',
+		borderRadius:'1rem',
+		padding:'1rem',
+		margin:'1rem',
+		bottom:'0',
+		left:'0',
+		width:'50%',
 		display:'grid',
 		gridTemplateColumns:'50% 50%'
 	}
 	return (
 		<div style={grid}>
 			<div>
+				<h2>Day:{game.day} of {game.length}</h2>
 				<p>{game.players[game.turn].name}</p>
 				<p>Seed:{game.players[game.turn].seed}</p>
 				<button onClick={turnChanger}>Change Turn</button>
